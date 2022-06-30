21d3e4094da502cd19137622242061b916dc252b
Hassan-Mehmood <89647174+Hassan-Mehmood@users.noreply.github.com>
2022-06-30 16:20:02 +0500

    Tabs section almost done

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,8 +1,8 @@
 const menuIcon = document.querySelector(".menu-icon");
 const navLinks = document.querySelector(".nav-links");
+const tabItems = document.querySelectorAll(".tab-item");
 
 menuIcon.addEventListener("click", changeMenuState);
-
 function changeMenuState() {
   if (menuIcon.src.includes("/images/icon-hamburger.svg")) {
     menuIcon.src = "./images/icon-close.svg";
@@ -13,6 +13,21 @@ function changeMenuState() {
     navLinks.classList.add("hide-menu");
     menuIcon.src = "./images/icon-hamburger.svg";
   }
-
   navLinks.classList.remove("hide-menu");
+}
+
+tabItems.forEach((item) => {
+  return item.addEventListener("click", selectItem);
+});
+
+function selectItem() {
+  removeBorder();
+
+  this.classList.add("active-border");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => {
+    item.classList.remove("active-border");
+  });
 }
